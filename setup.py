a2e07c9e4f6da6fcd0081c2bb327179b459ba34b
Diego Cantor <diego.cantor@gmail.com>
2018-10-12 13:24:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 import sys
 from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
+from setuptools import setup
 
 # Package meta-data.
 NAME = 'nrrd_tools'
@@ -18,19 +18,8 @@ REQUIRES_PYTHON = '>=2.7.0'
 VERSION = '1.0'
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-    'numpy', 'pynrrd', 'SimpleITK',
-]
-
-# What packages are optional?
-EXTRAS = {
-    
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
+REQUIRED = ['numpy', 'pynrrd', 'SimpleITK']
+EXTRAS = {}
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -47,43 +36,6 @@ about = {}
 about['__version__'] = VERSION
 
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-        
-        sys.exit()
-
-
 # Where the magic happens:
 setup(
     name=NAME,
@@ -95,25 +47,10 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    #packages=find_packages(exclude=('tests',)),
     # If your package is a single module, use this instead of 'packages':
-    py_modules=['nrrd_tools'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
+    packages=['nrrd_tools'],
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    license='MIT'
 )
